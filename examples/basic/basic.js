5e89e6b9eeee8c51a56446c5711180f97eaa2e38
Chris Mather <mather.chris@gmail.com>
2014-07-15 11:03:07 -0700

    fix ie8 onclick bug...
    In IE8 the jQuery event object for onclick has the "which" property always set
    to 0. Previously we were relying on it being 1 to indiciate a left mouse click.
    Also, it appears that right mouse clicks never fire the click event so not sure
    why we needed that code in the first place. Now it works in ie8 again without
    causing a page refresh.

--- examples/basic/basic.js
+++ examples/basic/basic.js
@@ -1,6 +1,13 @@
 if (Meteor.isClient) {
   Deps.autorun(function (c) {
     var value = Iron.Location.get();
-    console.log('Iron.Location.get(): ', value);
+    console.log('Iron.Location.get(): ', value && value.href);
+  });
+}
+
+if (Meteor.isServer) {
+  WebApp.connectHandlers.use(function (req, res, next) {
+    console.log(req.method + ': ' + req.url);
+    next();
   });
 }
