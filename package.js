c78832a30bb26b7fd9e010f7ebf17cbf477d28cf
Chris Mather <mather.chris@gmail.com>
2014-09-15 22:28:54 -0700

    bump version to 1.0.0-pre1

--- package.js
+++ package.js
@@ -1,20 +1,20 @@
 Package.describe({
   summary: 'Reactive urls that work in IE8/9 and modern browsers.',
-  version: '1.0.0-pre0',
+  version: '1.0.0-pre1',
   git: 'https://github.com/eventedmind/iron-location.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.2-rc0');
+  api.versionsFrom('METEOR@0.9.2');
   
   api.use('underscore');
   api.use('tracker');
   api.use('jquery');
 
-  api.use('iron:core@1.0.0-pre0');
+  api.use('iron:core@1.0.0-pre1');
   api.imply('iron:core');
 
-  api.use('iron:url@1.0.0-pre0');
+  api.use('iron:url@1.0.0-pre1');
 
   api.add_files('lib/utils.js', 'client');
   api.add_files('lib/state.js', 'client');
