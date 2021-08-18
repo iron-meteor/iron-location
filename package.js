2c8d8fe1f00eff047392ea27a5d393eb39d985da
jirkakrepl <jiri.krepl@gmail.com>
2021-08-18 15:35:42 +0200

    refactor: updated package to work with meteor 2.3

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
   git: 'https://github.com/eventedmind/iron-location.git'
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.2');
 
   api.use('underscore');
@@ -19,17 +19,17 @@ Package.on_use(function (api) {
 
   api.use('appcache', {weak: true});
 
-  api.add_files('lib/utils.js', 'client');
-  api.add_files('lib/state.js', 'client');
-  api.add_files('lib/location.js', 'client');
+  api.addFiles('lib/utils.js', 'client');
+  api.addFiles('lib/state.js', 'client');
+  api.addFiles('lib/location.js', 'client');
 
   api.export(['urlToHashStyle', 'urlFromHashStyle'], 'client', {testOnly: true});
 });
 
-Package.on_test(function (api) {
+Package.onTest(function (api) {
   api.use('iron:location');
   api.use('tinytest');
   api.use('test-helpers');
 
-  api.add_files('test/location_test.js', 'client');
+  api.addFiles('test/location_test.js', 'client');
 });
